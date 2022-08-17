53e4d518e3be9557660fc6f7e373922e6ee93ecb
Felix <barosiofelix@gmail.com>
2022-08-17 11:36:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const countDown = (num) => {
+    for(let i = num; i>=0; i--){
+        console.log(i)
+    }
+}
+
+// const writeCards = (message) => {
+//     return message.map(message => `Thank you, ${message}, for the wonderful surprise gift!`);
+// }
+
+function writeCards(message){
+    let cards = []
+    for(let i = 0; i < message.length; i++){
+        cards.push(`Thank you, ${message[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
